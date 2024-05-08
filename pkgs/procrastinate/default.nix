e5a2db5ad10cc23c23c88d14e22a625c78168adb
Burkhard Mittelbach <wasabi37a@googlemail.com>
2024-05-08 11:10:41 +0200

    Procrastinate v0.2.1

--- pkgs/procrastinate/default.nix
+++ pkgs/procrastinate/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
     pname = "procrastinate";
-    version = "0.2.0";
+    version = "0.2.1";
 
     src = fetchFromGitHub {
         owner = "Wasabi375";
         repo = pname;
         rev = version;
-        hash = "sha256-XQmKVY+XuhUY+hMm1+S/O8jC1uJmqSfsnSt52TqfT6A=";
+        hash = "sha256-hYWc27YdApT/YRbsyZkE2ywGuGJpLhNO6eechG4xlh0=";
     };
     
-    cargoHash = "sha256-nKbszDnlQknGY5aqLehIHbB73hmzxwRHdKTFVpMleKk=";
+    cargoHash = "sha256-pry7nC/oIX82Se7wRgKdiThC57LkhTEWSoIe2GYdglc=";
 
     meta = with lib; {
         description = "A suite of programs to send time delayed notifications";
