0c682f8ba0d26994c4fff93d8185a2d19b614420
Burkhard Mittelbach <wasabi37a@googlemail.com>
2025-03-17 16:12:17 +0100

    Build procrastinate v0.6.1 with workspaces

--- pkgs/procrastinate/default.nix
+++ pkgs/procrastinate/default.nix
@@ -1,17 +1,44 @@
-{lib, fetchFromGitHub, rustPlatform}:
-
-rustPlatform.buildRustPackage rec {
-    pname = "procrastinate";
-    version = "v0.5.0";
-
-    src = fetchFromGitHub {
-        owner = "Wasabi375";
-        repo = pname;
-        rev = version;
-        hash = "sha256-PEyvG/E0CiJqJIHMPMFFJhCADrbEfn84g3y8VoSKoKQ=";
+{lib, stdenv, fetchFromGitHub, rustPlatform}:
+let 
+    version = "v0.6.1";
+    proc-bin = member: rustPlatform.buildRustPackage rec {
+       pname = "procrastinate";
+       inherit version;
+   
+       src = fetchFromGitHub {
+           owner = "Wasabi375";
+           repo = pname;
+           rev = version;
+           hash = "sha256-5VR/k9vSrOnAyszOvXNmaneER9dkOhsevhNQTFNBrDU=";
+       };
+       
+       cargoHash = "sha256-/OzgMkK3Em9SJD7NYUgmOu89E4j/FSAGfHa6Mj1AYnQ=";
+   
+       buildAndTestSubdir = "${member}";
     };
-    
-    cargoHash = "sha256-vc8THTNTpYXWy2THlYkTOFdvLjy92LYY8omm1IfN7sc=";
+    cli = proc-bin "procrastinate";
+    xtask = proc-bin "xtask";
+    daemon = proc-bin "daemon";
+    work = proc-bin "work";
+in stdenv.mkDerivation {
+    pname = "procrastinate";
+    inherit version;
+
+    unpackPhase = ''
+        runHook preUnpack
+        runHook postUnpack
+    '';
+
+    installPhase = ''
+        runHook preInstall
+        mkdir -p $out/bin
+        ln -s ${cli}/bin/procrastinate $out/bin/procrastinate
+        ln -s ${daemon}/bin/procrastinate-daemon $out/bin/procrastinate-daemon
+        ln -s ${work}/bin/procrastinate-work $out/bin/procrastinate-work
+        ${xtask}/bin/xtask --out-dir $out man
+        ${xtask}/bin/xtask --out-dir $out completion
+        runHook postInstall
+    '';
 
     meta = with lib; {
         description = "A suite of programs to send time delayed notifications";
