2c0f16233530b02979ad26d9f2b5ba7b8972888d
Burkhard Mittelbach <wasabi37a@googlemail.com>
2025-11-09 19:51:24 +0100

    remove "unstable" branch. This now always follows stable nixos

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
   description = "My personal nix repository";
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
-    unstable.url = "nixpkgs/nixos-unstable";
-  };
-  
-  outputs = { self, nixpkgs, unstable, ... }: 
-   let
+  inputs = { nixpkgs.url = "nixpkgs/nixos-25.05"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let
       systems = [
         "x86_64-linux"
         "i686-linux"
@@ -17,29 +14,30 @@
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in {
-    legacyPackages = forAllSystems (system: import ./default.nix {
-      pkgs = import nixpkgs { inherit system; };
-    });
-    packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
-    overlays.default = final: prev: {
-      wasabi375 = import ./default.nix {
-        wasabipkgs = prev;
-        pkgs = prev;
-      };
-    };
-    nixosModules.wasabi375 = { lib, pkgs, ... }: {
-      options.wasabi375 = lib.mkOption {
-        type = lib.mkOptionType {
-          name = "wasabi375";
-          description = "My custom package repository";
-          check = builtins.isAttrs;
-        };
-        description = "Use this option to import packages from wasabi375";
-        default = import self {
-          wasabipkgs = pkgs;
-          pkgs = pkgs;
+      legacyPackages = forAllSystems (system:
+        import ./default.nix { pkgs = import nixpkgs { inherit system; }; });
+      packages = forAllSystems (system:
+        nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v)
+        self.legacyPackages.${system});
+      overlays.default = final: prev: {
+        wasabi375 = import ./default.nix {
+          wasabipkgs = prev;
+          pkgs = prev;
         };
       };
+      nixosModules.wasabi375 = { lib, pkgs, ... }: {
+        options.wasabi375 = lib.mkOption {
+          type = lib.mkOptionType {
+            name = "wasabi375";
+            description = "My custom package repository";
+            check = builtins.isAttrs;
+          };
+          description = "Use this option to import packages from wasabi375";
+          default = import self {
+            wasabipkgs = pkgs;
+            pkgs = pkgs;
+          };
+        };
+      };
     };
-  };
 }
